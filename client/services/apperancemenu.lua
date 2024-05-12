373f51868ee38adef1075c4cab3f3075efa9700f
jakeyboi1 <jakelandrum2004@gmail.com>
2024-05-11 19:12:13 -0500

    unusedFileRemoved

--- client/services/apperancemenu.lua
+++ client/services/apperancemenu.lua
@@ -3,27 +3,6 @@ CamZ = Config.CameraCoords.creation.z + 0.5
 local Gender = GetGender()
 ActiveTexture, ActiveColor1, ActiveColor2, ActiveColor3, ActiveOpacity, ActiveVariant = {}, {}, {}, {}, {},{}
 
-MyMenu = FeatherMenu:RegisterMenu('feather:character:menu', {
-    top = '1%',
-    left = '1%',
-    ['720width'] = '500px',
-    ['1080width'] = '600px',
-    ['2kwidth'] = '700px',
-    ['4kwidth'] = '900px',
-    style = {
-    },
-    contentslot = {
-        style = { --This style is what is currently making the content slot scoped and scrollable. If you delete this, it will make the content height dynamic to its inner content.
-            ['height'] = '700px',
-            ['width'] = '500px',
-            ['min-height'] = '500px'
-        }
-    },
-
-    draggable = false,
-    canclose = true
-})
-
 MainAppearanceMenu = MyMenu:RegisterPage('appearance:page')
 FaceActive = nil
 SelectedAttributes = {} -- This can keep track of what was selected data wise
