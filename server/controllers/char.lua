ff730c61a37ff69c1f869f057aa7c3fc47c98fc6
jakeyboi1 <jakelandrum2004@gmail.com>
2024-05-31 11:47:27 -0500

    ?Params

--- server/controllers/char.lua
+++ server/controllers/char.lua
@@ -1,7 +1,7 @@
 CharControllers = {}
 
 function CharControllers.GetCharApperanceData(charId)
-    local result = MySQL.query.await("SELECT * FROM character_appearance WHERE id = @id", { ['id'] = charId })
+    local result = MySQL.query.await("SELECT * FROM character_appearance WHERE id = ?", { charId })
     return result[1]
 end
 
@@ -10,6 +10,6 @@ function CharControllers.UpdateCharApperanceData(charId, attributes, clothing, o
 end
 
 function CharControllers.GetCharIdFromUserId(userId)
-    local result = MySQL.query.await("SELECT id FROM characters WHERE user_id = @user_id ORDER BY user_id DESC", { ['user_id'] = userId })
+    local result = MySQL.query.await("SELECT id FROM characters WHERE user_id = ? ORDER BY user_id DESC", { userId })
     return result[#result].id
 end
